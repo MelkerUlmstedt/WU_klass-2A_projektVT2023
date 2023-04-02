4b25e2e8e8469d73349050a4458feffa56f71bd3
MelkerUlmstedt <melker.ulmstedt2@elev.ga.ntig.se>
2023-04-02 22:39:10 +0200

    summary
    klar med 3e sidan

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const burger = document.getElementById('burger')
-const nav = document.getElementById('nav-links')
-
-function toggleMenu() {
-    burger.classList.toggle('burger_toggle')
-    nav.classList.toggle('nav_active')
-}
-
-burger.addEventListener('click', toggleMenu)
